c50208dac3421f0d0530bddb718f99d15533ef47
Your NameNikhilsingRajput <nikhilsingrajput2016@gmail.com>
2023-02-27 22:52:07 +0530

    h

--- src/Newpost.js
+++ src/Newpost.js
@@ -1,19 +1,52 @@
-
+import { useState } from "react";
+import Axios from "axios"
 
 function Newpost(){
-    return <form action="post" >
+//     const url = "" ;
+//     const [data , setdata] = useState({
+//             "name":"Nick",
+//             "location":"",
+//             "description": "",
+//             "PostImage": "",
+//             "date" : "new date()"
+//     })
+
+//     function submit(e){
+//         e.preventDefault();
+//         Axios.post(url,{
+//             name: data.name,
+//             location: data.location,
+//             likes:0,
+//             description: data.description,
+//             PostImage:data.PostImage,
+//             data:data.date
+
+//         }).then((res)=>{
+//             console.log(res.data)
+//         })
+//     }
+
+//     function handle(e){
+//         const newdata = { ...data}
+//         newdata[e.target.id] = e.target.value
+//         setdata(newdata)
+//         console.log(newdata)
+//     }
+
+
+    return <form onSubmit={(e)=>{submit(e)}} >
         <div >
-        <input type="file" required />
+        <input onChange={(e)=>handle(e)} id="PostImage" type="text" required />
         </div>
         <div >
-            <input typeof="text" placeholder="Author" required/>
+            <input onChange={(e)=>handle(e)} id="name" type="text" placeholder="Author" required/>
             <span>
-                <input typeof="text" placeholder="Location" required/>
+             <input onChange={(e)=>handle(e)} id="location" type="text" placeholder="Location" required/>
             </span>
         </div>
         
         <div>
-            <input typeof="text" placeholder="Description"/>
+            <input type="text" onChange={(e)=>handle(e)} id="description" placeholder="Description" required/>
 
         </div>
         <button type="submit">Post</button>
